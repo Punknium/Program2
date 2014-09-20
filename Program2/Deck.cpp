8e5006a3587c01bfb374be09c32f9c7070508a79
finlayir1 <finlayir1@gcc.edu>
2014-09-20 19:13:32 -0400

    just marked down what files i'm working on

--- Program2/Deck.cpp
+++ Program2/Deck.cpp
@@ -1,3 +1,10 @@
+/*
+  File:			Deck.cpp
+  Author:		Ian Finlay
+  Course:		COMP 220, Computer Programming II
+  Date:			9/20/2014
+  Description:  This file provides the functions for the hearts game.
+*/
 #include "stdafx.h"
 #include "Deck.h"
 
